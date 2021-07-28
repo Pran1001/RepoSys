c08de6a15b00169794f044cda8075abb62aa42e9
Pranali Darekar <pranalidarekar99@gmail.com>
2021-07-28 20:30:21 +0530

    Frontend
    Completed Frontend till Registration page

---
+++ REPOSYS/REPOSYSAPP/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def forget(request):
+    return render(request, 'forgetpass.html')
+
