9d80a9bd9f51f098ecd04bd868ba64606c773e91
Riddhi Warale <warale.riddhi22@gmail.com>
2021-08-04 19:11:22 +0530

    registration
    data goes to back end from front end

--- REPOSYS/REPOSYSAPP/models.py
+++ REPOSYS/REPOSYSAPP/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Student(models.Model):
-    username = models.CharField(max_length=225)
+    username = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     full_name = models.CharField(max_length=225, null=True)
     roll_no = models.CharField(max_length=225, null=True)
     email = models.EmailField(max_length=225, null=True)
@@ -12,4 +13,6 @@ class Student(models.Model):
 
     def __str__(self):
         return self.full_name
+
+
 
--- REPOSYS/REPOSYSAPP/views.py
+++ REPOSYS/REPOSYSAPP/views.py
@@ -1,24 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from .forms import StudentRegisterForm
+from .forms import StudentRegisterForm, UserForm
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+from .models import Student
+
 
 def home(request):
     return render(request, 'home.html')
 
 def register(request):
-    student_form = StudentRegisterForm()
+    form = StudentRegisterForm()
+    user_form = UserForm()
     if request.method == 'POST':
+        user_form = UserForm(request.POST)
         form = StudentRegisterForm(request.POST)
-        if form.is_valid():
+        if form.is_valid() and user_form.is_valid():
+            username = user_form.cleaned_data.get('username')
+            user_form.save()
             form.save()
+            student = Student.objects.get(roll_no=username)
+            student.username = User.objects.get(username=username)
+            student.save()
+            return redirect('login')
 
-    context = {'form': form}
+    context = {'form': form, 'user_form': user_form}
     return render(request, 'register.html', context)
 
-    context = {}
-    return render(request, 'register.html')
+
 
 def profile(request):
     return render(request, 'profile.html')
