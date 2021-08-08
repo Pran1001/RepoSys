e68fbe9bde69112611fff56c600e18ea6dcb46f5
Pran1001 <pranalidarekar99@gmail.com>
2021-08-08 13:20:12 +0530

    Report page frontend completed
    Front end customization

--- REPOSYS/REPOSYSAPP/views.py
+++ REPOSYS/REPOSYSAPP/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from .forms import StudentRegisterForm, UserForm
 from django.contrib.auth.models import User
 
